7010659579647d953e01aed1851010d7a737e1b9
Ilham Zukhri <ilhamzukhri354@gmail.com>
2025-05-03 20:47:22 +0700

    create database

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // \App\Models\User::factory(10)->create();
+        \App\Models\User::create([
+            'username' => 'admin',
+            'email' => 'admin@gmail.com',
+            'role' => 'super-admin',
+            'password' => bcrypt('kunci123'),
+            'no_wa' => '0823211342345',
+        ]);
+    }
+}
