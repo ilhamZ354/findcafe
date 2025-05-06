3932ac4c66fee2400cae8b87093ec77c21991185
Ilham Zukhri <ilhamzukhri354@gmail.com>
2025-05-06 08:01:01 +0700

    feat: routing

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,7 @@ Route::get('/', function () {
 });
 
 Route::post('/register', [AuthController::class, 'store'])->name('register');
+Route::post('/register-cafe', [AuthController::class, 'storeCafe'])->name('register.cafe');
 Route::post('/login', [AuthController::class, 'login'])->name('login');
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
