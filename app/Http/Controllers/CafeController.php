642940798a4ad2f844df0af8f0109617e740bbe0
MuhammadBobby <m.bobbyoktaviano@gmail.com>
2025-05-15 08:28:20 +0700

    ..

--- app/Http/Controllers/CafeController.php
+++ app/Http/Controllers/CafeController.php
@@ -119,7 +119,8 @@ class CafeController extends Controller
     }
 
     // list cafe untuk user
-    public function listCafes() {
+    public function listCafes()
+    {
         $data = DB::table('users')
             ->join('cafe_details', 'users.id', '=', 'cafe_details.cafe_id')
             ->join('rating_reviews', 'cafe_details.cafe_id', '=', 'rating_reviews.cafe_id')
@@ -138,23 +139,23 @@ class CafeController extends Controller
 
         // dd($data);
         return view('pages.index', compact('data'));
-
     }
 
-    public function show($cafe) {
+    public function show($cafe)
+    {
 
         $data = DB::table('users')
-        ->join('cafe_details', 'users.id', '=', 'cafe_details.cafe_id')
-        ->join('rating_reviews', 'cafe_details.id', '=', 'rating_reviews.cafe_id')
-        ->where('users.id', $cafe) // cari dari id
-        ->select(
-            'users.id',
-            'users.username',
-            'users.email',
-            'cafe_details.*',
-            'rating_reviews.*'
-        )
-        ->first();
+            ->join('cafe_details', 'users.id', '=', 'cafe_details.cafe_id')
+            ->join('rating_reviews', 'cafe_details.id', '=', 'rating_reviews.cafe_id')
+            ->where('users.id', $cafe) // cari dari id
+            ->select(
+                'users.id',
+                'users.username',
+                'users.email',
+                'cafe_details.*',
+                'rating_reviews.*'
+            )
+            ->first();
 
         // dd($data);
 
--- resources/views/components/home/services.blade.php
+++ resources/views/components/home/services.blade.php
@@ -33,69 +33,75 @@
                             </h4>
                         </div>
 
-                        <div class="grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3">
-                            @foreach ($data as $cafe)
-                                <div class="w-full max-w-md bg-white border border-gray-200 rounded-lg shadow-sm">
-                                    <a href="{{ route('detail-cafe', $cafe->cafe_detail_id) }}">
-                                        <img class="rounded-t-lg" src="{{ $cafe->image_profile }}"
-                                            alt="product image" />
-                                    </a>
-                                    <div class="px-5 py-5">
-                                        <a href="{{ route('detail-cafe', $cafe->id) }}">
-                                            <h5 class="text-xl font-semibold tracking-tight text-gray-900">
-                                                {{ $cafe->username }}</h5>
-                                            <p class="mt-2 text-sm text-grayTheme">
-                                                {{ Str::words($cafe->description, 20, '...') }}</p>
-                                            <p class="mt-2 text-sm text-lightGrayTheme">Lokasi : <span
-                                                    class="text-gray-700">{{ $cafe->address }}</span></p>
+                        @if (count($data) > 0)
+                            <div class="grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3">
+                                @foreach ($data as $cafe)
+                                    <div class="w-full max-w-md bg-white border border-gray-200 rounded-lg shadow-sm">
+                                        <a href="{{ route('detail-cafe', $cafe->cafe_detail_id) }}" class="block">
+                                            <img class="object-cover w-full rounded-t-lg h-60"
+                                                src="{{ $cafe->image_profile }}" alt="product image" />
                                         </a>
-                                        <div class="flex items-center mt-2.5 mb-5">
-                                            <div class="flex items-center space-x-1 rtl:space-x-reverse">
-                                                <svg class="w-4 h-4 text-yellow-300" aria-hidden="true"
-                                                    xmlns="http://www.w3.org/2000/svg" fill="currentColor"
-                                                    viewBox="0 0 22 20">
-                                                    <path
-                                                        d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-                                                </svg>
-                                                <svg class="w-4 h-4 text-yellow-300" aria-hidden="true"
-                                                    xmlns="http://www.w3.org/2000/svg" fill="currentColor"
-                                                    viewBox="0 0 22 20">
-                                                    <path
-                                                        d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-                                                </svg>
-                                                <svg class="w-4 h-4 text-yellow-300" aria-hidden="true"
-                                                    xmlns="http://www.w3.org/2000/svg" fill="currentColor"
-                                                    viewBox="0 0 22 20">
-                                                    <path
-                                                        d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-                                                </svg>
-                                                <svg class="w-4 h-4 text-yellow-300" aria-hidden="true"
-                                                    xmlns="http://www.w3.org/2000/svg" fill="currentColor"
-                                                    viewBox="0 0 22 20">
-                                                    <path
-                                                        d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-                                                </svg>
-                                                <svg class="w-4 h-4 text-gray-200 dark:text-gray-600" aria-hidden="true"
-                                                    xmlns="http://www.w3.org/2000/svg" fill="currentColor"
-                                                    viewBox="0 0 22 20">
-                                                    <path
-                                                        d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-                                                </svg>
+                                        <div class="px-5 py-5">
+                                            <a href="{{ route('detail-cafe', $cafe->id) }}">
+                                                <h5 class="text-xl font-semibold tracking-tight text-gray-900">
+                                                    {{ $cafe->username }}</h5>
+                                                <p class="mt-2 text-sm text-grayTheme">
+                                                    {{ Str::words($cafe->description, 20, '...') }}</p>
+                                                <p class="mt-2 text-sm text-lightGrayTheme">Lokasi : <span
+                                                        class="text-gray-700">{{ $cafe->address }}</span></p>
+                                            </a>
+                                            <div class="flex items-center mt-2.5 mb-5">
+                                                <div class="flex items-center space-x-1 rtl:space-x-reverse">
+                                                    <svg class="w-4 h-4 text-yellow-300" aria-hidden="true"
+                                                        xmlns="http://www.w3.org/2000/svg" fill="currentColor"
+                                                        viewBox="0 0 22 20">
+                                                        <path
+                                                            d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                                                    </svg>
+                                                    <svg class="w-4 h-4 text-yellow-300" aria-hidden="true"
+                                                        xmlns="http://www.w3.org/2000/svg" fill="currentColor"
+                                                        viewBox="0 0 22 20">
+                                                        <path
+                                                            d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                                                    </svg>
+                                                    <svg class="w-4 h-4 text-yellow-300" aria-hidden="true"
+                                                        xmlns="http://www.w3.org/2000/svg" fill="currentColor"
+                                                        viewBox="0 0 22 20">
+                                                        <path
+                                                            d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                                                    </svg>
+                                                    <svg class="w-4 h-4 text-yellow-300" aria-hidden="true"
+                                                        xmlns="http://www.w3.org/2000/svg" fill="currentColor"
+                                                        viewBox="0 0 22 20">
+                                                        <path
+                                                            d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                                                    </svg>
+                                                    <svg class="w-4 h-4 text-gray-200 dark:text-gray-600"
+                                                        aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                                                        fill="currentColor" viewBox="0 0 22 20">
+                                                        <path
+                                                            d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                                                    </svg>
+                                                </div>
+                                                <span
+                                                    class="bg-blue-100 text-blue-800 text-xs font-semibold px-2.5 py-0.5 rounded-smms-3">5.0</span>
                                             </div>
-                                            <span
-                                                class="bg-blue-100 text-blue-800 text-xs font-semibold px-2.5 py-0.5 rounded-smms-3">5.0</span>
-                                        </div>
-                                        <div class="flex items-center justify-end">
-                                            <a href="{{ route('detail-cafe', $cafe->id) }}"
-                                                class="text-white bg-primaryBrown hover:bg-semiPrimaryBrown focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center ">Lihat
-                                                lainnya</a>
+                                            <div class="flex items-center justify-end">
+                                                <a href="{{ route('detail-cafe', $cafe->id) }}"
+                                                    class="text-white bg-primaryBrown hover:bg-semiPrimaryBrown focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center ">Lihat
+                                                    lainnya</a>
+                                            </div>
                                         </div>
                                     </div>
-                                </div>
 
-                                {{-- </a> --}}
-                            @endforeach
-                        </div>
+                                    {{-- </a> --}}
+                                @endforeach
+                            </div>
+                        @else
+                            <div class="flex items-center justify-center">
+                                <p class="text-center text-white">Tidak ada data</p>
+                            </div>
+                        @endif
                     </div>
                     <!-- ====== Table One End -->
                 </div>
